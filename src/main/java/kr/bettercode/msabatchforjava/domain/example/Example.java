326b35fdb87116cc15eaae5ee32c3d48cb60ce58
Dion <ksundong@gmail.com>
2021-12-28 20:24:42 +0900

    feat: 예제 데이터 도메인 객체 생성

---
+++ src/main/java/kr/bettercode/msabatchforjava/domain/example/Example.java
@@ -0,0 +1,13 @@
+package kr.bettercode.msabatchforjava.domain.example;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Example {
+
+  private Long id;
+  private LocalDate date;
+  private LocalTime time;
+  private String title;
+  private String content;
+}
---
+++ src/main/java/kr/bettercode/msabatchforjava/domain/examplesummary/ExampleSummary.java
@@ -0,0 +1,10 @@
+package kr.bettercode.msabatchforjava.domain.examplesummary;
+
+import java.time.LocalDateTime;
+
+public class ExampleSummary {
+
+  private Long id;
+  private LocalDateTime datetime;
+  private String summary;
+}
