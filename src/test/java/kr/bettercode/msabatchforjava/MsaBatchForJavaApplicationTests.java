431d982658a1701fb0a7e61084a0b856df9a1577
Dion <ksundong@gmail.com>
2021-12-31 12:35:55 +0900

    test: batch 통합테스트 추가

--- src/test/java/kr/bettercode/msabatchforjava/MsaBatchForJavaApplicationTests.java
+++ src/test/java/kr/bettercode/msabatchforjava/MsaBatchForJavaApplicationTests.java
@@ -1,13 +1,50 @@
 package kr.bettercode.msabatchforjava;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@SpringBatchTest
 @SpringBootTest
 class MsaBatchForJavaApplicationTests {
 
+  @Autowired
+  JobLauncherTestUtils jobLauncherTestUtils;
+
+  @Autowired
+  JdbcTemplate jdbcTemplate;
+
   @Test
-  void contextLoads() {
+  @DisplayName("Batch Job이 실행되고, 해당 Job이 완료되면 Job과 Step은 COMPLETED 상태가 된다.")
+  void jobExecutionTest() throws Exception {
+    // given
+    final Integer beforeJobCount = getExampleSummaryCount();
+
+    // when
+    final JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+    final Integer afterJobCount = getExampleSummaryCount();
+
+    // then
+    assertAll(
+        () -> assertThat(jobExecution.getExitStatus().getExitCode()).isEqualTo("COMPLETED"),
+        () -> assertThat(jobExecution.getStepExecutions())
+            .extracting(stepExecution -> stepExecution.getExitStatus().getExitCode())
+            .hasSize(2)
+            .containsExactly("COMPLETED", "COMPLETED"),
+        () -> assertThat(beforeJobCount).isEqualTo(0),
+        () -> assertThat(afterJobCount).isEqualTo(6)
+    );
   }
 
+  private Integer getExampleSummaryCount() {
+    return jdbcTemplate.queryForObject("SELECT COUNT(*) FROM example_summary", Integer.class);
+  }
 }
